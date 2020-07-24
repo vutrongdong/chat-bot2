3dd1522fa5313149ebc28a216e249ca0d748eda7
Vu Trong Dong <trongdong717@gmail.com>
2020-07-24 11:55:36 +0700

    first

--- src/api.js
+++ src/api.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const JSONParseError = require('@line/bot-sdk').JSONParseError
+const SignatureValidationFailed = require('@line/bot-sdk').SignatureValidationFailed
+const bodyParser= require('body-parser');
+const cors      = require('cors');
 
 const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
-});
+app.use((err, req, res, next) => {
+  if (err instanceof SignatureValidationFailed) {
+    res.status(401).send(err.signature)
+    return
+  } else if (err instanceof JSONParseError) {
+    res.status(400).send(err.raw)
+    return
+  }
+  next(err) // will throw default 500
+})
 
-app.use(`/.netlify/functions/api`, router);
+app.use('/api', require('./routers'));
 
 module.exports = app;
 module.exports.handler = serverless(app);
